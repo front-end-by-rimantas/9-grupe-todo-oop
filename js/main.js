ea8c19b620055ef16727a87df72d31ae980bbda3
Rimantas <belauzas@yahoo.com>
2019-07-29 12:47:34 +0300

    Zmogus class

--- js/main.js
+++ js/main.js
@@ -4,5 +4,38 @@ import Todo from './todo.js';
 const td = new Todo('#tasks');
 // const td2 = new Todo('#tasks2');
 
-console.log( td );
+// console.log( td );
 // console.log( td2 );
+
+
+
+
+
+class Zmogus {
+    constructor ( vardas, amziu ) {
+        this.vardas = vardas;
+        this.amzius = amziu;
+    }
+
+    labas = () => {
+        return `Labas, as esu ${this.vardas}!`;
+    }
+
+    arPilnametis = () => {
+        if ( this.amzius >= 18 ) {
+            return `Taip, ${this.vardas} yra pilnametis/-e.`;
+        } else {
+            return `Ne, ${this.vardas} nera pilnametis/-e.`;
+        }
+    }
+}
+
+let jonas = new Zmogus('Jonas', 99);
+let maryte = new Zmogus('Maryte', 16);
+
+console.log(jonas);
+console.log(jonas.labas());
+console.log(jonas.arPilnametis());
+console.log(maryte);
+console.log(maryte.labas());
+console.log(maryte.arPilnametis());

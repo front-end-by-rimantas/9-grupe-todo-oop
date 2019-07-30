a9af580bbd6e18d6346d26f761a1fa6d37c91bbe
Rimantas <belauzas@yahoo.com>
2019-07-30 10:06:52 +0300

    Task filter options

--- js/todo.js
+++ js/todo.js
@@ -3,10 +3,12 @@
 class TodoClass {
     constructor ( target ) {
         this.target = document.querySelector(target);
+        this.filtersHTML;
         this.nextId = 1;
         this.textarea;
         this.taskListElement;
         this.tasks = [];
+        this.filter = 'in progress';       // all, done, in progress
 
         this.HTML_template = `<div class="tasker">
                                 <h2>Tasku sarasas</h2>
@@ -15,12 +17,18 @@ class TodoClass {
                                     <div class="btn save">Save</div>
                                     <div class="btn cancel">Cancel</div>
                                 </form>
+                                <div class="filter">
+                                    <div class="option" data-filter="all">All</div>
+                                    <div class="option" data-filter="done">Done</div>
+                                    <div class="option active" data-filter="in-progress">In progress</div>
+                                </div>
                                 <div class="task-list">task list...</div>
                             </div>`;
         this.HTML_empty = `<div class="empty">Sorry, no tasks</div>`;
         this.HTML_task = `<div class="task-item" data-task-id="{{id}}">
                             <header>
                                 <h3>#{{id}}</h3>
+                                <span class="btn done">Archive</span>
                                 <span class="btn edit">Edit</span>
                                 <span class="btn delete">Delete</span>
                             </header>
@@ -36,10 +44,26 @@ class TodoClass {
         // selecting elements
         this.textarea = this.target.querySelector('textarea');
         this.taskListElement = this.target.querySelector('.task-list');
+        this.filtersHTML = this.target.querySelectorAll('.option');
         // adding events
         this.target.querySelector('.save').addEventListener('click', this.addTask);
         this.target.querySelector('.cancel').addEventListener('click', this.cancelTask);
+        this.filtersHTML.forEach( option => {
+            option.addEventListener('click', this.updateFilter)
+        });
         this.renderTasks();
+    }
+
+    updateFilter = ( event ) => {
+        this.filter = event.target.dataset.filter;
+
+        this.filtersHTML.forEach( option => {
+            if ( option.dataset.filter === this.filter ) {
+                option.classList.add('active');
+            } else {
+                option.classList.remove('active');
+            }
+        });
     }
 
     addTask = () => {
@@ -49,7 +73,8 @@ class TodoClass {
         }
         this.tasks.push( {
             id: this.nextId,
-            text: text
+            text: text,
+            done: false
         } );
         this.nextId++;
         
@@ -61,14 +86,8 @@ class TodoClass {
     }
 
     deleteTask = ( event ) => {
-        const id = event.path[2].dataset.taskId;
+        const id = parseInt(event.path[2].dataset.taskId);        
         this.tasks = this.tasks.filter( task => task.id !== id );
-
-
-        console.log( this.tasks );
-        console.log( this.tasks.filter( task => task.id !== id ) );
-        
-
         this.renderTasks();
     }
 
@@ -77,20 +96,24 @@ class TodoClass {
             this.taskListElement.innerHTML = this.HTML_empty;
         } else {
             // issivalome HTML, kai atsiranda pirmas task'as
-            if ( this.tasks.length === 1 ) {
-                this.taskListElement.innerHTML = '';
-            }
-            // naujas elementas
-            let task_temp_HTML = this.HTML_task;
-            task_temp_HTML = task_temp_HTML.replace('{{id}}', this.nextId - 1)
-                                            .replace('{{id}}', this.nextId - 1)
-                                            .replace('{{text}}', this.textarea.value);
-            // esamas.push(naujas)
-            this.taskListElement.insertAdjacentHTML('beforeend', task_temp_HTML);
+            let full_HTML = '',
+                task_HTML = '';
 
-            // naujam sukurtam task'ui uzdedame event listener'ius
-            this.target.querySelector(`.task-item[data-task-id="${this.nextId - 1}"] .delete`)
-                        .addEventListener('click', this.deleteTask)
+            this.tasks.forEach( task => {
+                task_HTML = this.HTML_task;
+                task_HTML = task_HTML.replace('{{id}}', task.id)
+                                    .replace('{{id}}', task.id)
+                                    .replace('{{text}}', task.text);
+                full_HTML += task_HTML;
+            });
+
+            // update task list html
+            this.taskListElement.innerHTML = full_HTML;
+
+            // select all tasks and add event listeners
+            this.target.querySelectorAll('.task-list .task-item').forEach( task => {
+                task.querySelector('.delete').addEventListener('click', this.deleteTask);
+            });
         }
     }
 
